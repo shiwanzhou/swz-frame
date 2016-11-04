e3e9867db2d1571fc0c6536cb922470f7ec9182e
737816277@qq.com <shiwz@GlobalLink.cis.com.cn>
2016-11-04 18:45:24 +0800

    sw

--- swz-demo/module/js/swz2.js
+++ swz-demo/module/js/swz2.js
@@ -6,6 +6,16 @@
     var SWZ = function(selector, context) {
         return  new SWZ.prototype.init(selector,context);
     }
+    /***********javascript 底层补丁**************/
+    var objectP = Object.prototype;
+    var ohasOwn = objectP.hasOwnProperty;
+    var serialize = objectP.toString;
+    if(!SWZ.isArray){
+        SWZ.isArray = function(n){
+            return objectP.toString.call(n) === "[object Array]";
+        }
+    }
+    /***********主方法**************/
     SWZ.fn =  SWZ.prototype = {
         init: function( selector, context, rootjQuery ) {
             var that = this;
@@ -29,9 +39,6 @@
            return this.textContent;
         }
     };
-
-
-
     SWZ.prototype.init.prototype = SWZ.prototype;
     window.$ = SWZ;
 })(window);
