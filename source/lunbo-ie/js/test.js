29e7182c3a6346cc72207e5aa46ccfcde9073e4a
shiwz <shiwz@ourgame.com>
2016-11-28 10:53:30 +0800

    lunbo

--- source/lunbo-ie/js/test.js
+++ source/lunbo-ie/js/test.js
@@ -5,25 +5,69 @@
         var ImageLunbo = function(ele, opt) {
             this.$element = ele;
             this.defaultVal = {
-
+                iCenter:1,
+                styleArr : [
+                    {index:0,width:377, height:299, top:23, left:27, zIndex:1},
+                    {index:1,width:517, height:410, top:111, left:277, zIndex:4},
+                    {index:2,width:377, height:299, top:45, left:622, zIndex:2}
+                ]
             };
             this.options = $.extend({}, this.defaultVal, opt);
         };
         //定义ImageLunbo的方法
         ImageLunbo.prototype = {
             init: function(options,callback) {
-                var that = this;
-                console.log(22)
-
-
-
+                var _this = this;
+                this.prev =  $(this.$element).find(".prev");
+                this.next =  $(this.$element).find(".next");
+                this._doPrev = function () {return _this.doPrev.apply(_this)};
+                this._doNext = function () {return _this.doNext.apply(_this)};
+                this.iCenter =  this.defaultVal.iCenter;
+                this.options = this.defaultVal.styleArr;
+                this.setUp();
+                this.doImgClick();
+                this.prev.on("click",this._doPrev);
+                this.next.on("click",this._doNext);
             },
-            prev:function(){
-
-
+            setUp:function(){
+                var that =this;
+                if($(this.$element).find("li").length){
+                    $(this.$element).find("li").each(function(i){
+                        var pop = that.options[i];
+                        $(this).animate(pop);
+                    });
+                }
             },
-            next:function(){
-
+            doImgClick:function(){
+                var _this = this;
+                if($(this.$element).find("li").length){
+                    $(this.$element).find("li").each(function(i){
+                        $(this).on("click",function(){
+                            if (_this.options[i].index > _this.iCenter)
+                            {
+                                for (var j = 0; j < _this.options[i].index - _this.iCenter; j++) {
+                                    _this.options.unshift(_this.options.pop());
+                                }
+                                _this.setUp();
+                            }
+                            else if(_this.options[i].index < _this.iCenter)
+                            {
+                                for (var j = 0; j <  _this.iCenter - _this.options[i].index; j++) {
+                                    _this.options.push(_this.options.shift());
+                                }
+                                _this.setUp();
+                            }
+                        })
+                    });
+                }
+            },
+            doPrev:function(){
+                this.options.unshift(this.options.pop());
+                this.setUp();
+            },
+            doNext:function(){
+                this.options.push(this.options.shift());
+                this.setUp();
             }
         };
         //在插件中使用imageLunbo对象
@@ -36,7 +80,7 @@
     })(jQuery, window, document);
 
     $(function(){
-        $("#imageLunbo").imageLunbo()
+        $("#box").imageLunbo()
     });
 
 
